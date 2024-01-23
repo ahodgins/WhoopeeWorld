945913fd3a565be737468cc8840ebc1e102ef87b
Adam Hodgins <75464641+ahodgins@users.noreply.github.com>
2024-01-22 23:04:18 -0800

    Initial push to repo

---
+++ scripts/player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+class_name Player
+
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 169
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+		
+		
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+			jump(jump_force)
+			$AudioStreamPlayer2D.play()
+			
+		
+	var direction = Input.get_axis("move_left","move_right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+	velocity.x = direction * speed
+	move_and_slide()
+	
+	update_animations(direction)
+	
+	
+func jump(force):
+	velocity.y = -force
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+
