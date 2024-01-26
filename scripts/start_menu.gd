fbd99561ee4a7f1e31358b3508829673358632a5
Adam Hodgins <75464641+ahodgins@users.noreply.github.com>
2024-01-26 12:56:46 -0800

    added song to menu

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,9 +1,15 @@
 extends Control
 
 
+func _ready():
+	$ThemeSong.play()
+	
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
+	
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
